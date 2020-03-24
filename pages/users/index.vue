d4a61ccf97918e4304d8715fb87af8cb6a171b6b
Shai Gerbi <ShaiGerbi@gmail.com>
2020-03-24 19:33:11 +0200

    Users loop & error

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="container">
 
+        <div v-if="users.length === 0" class="box bg-yellow">
+            <p>No users exist</p>
+        </div>
+
+        <div v-if="users.length !== 0" class="box">
+            <ol>
+                <li v-for="user of users" :key="user.id">
+                    <nuxt-link :to="{ name: 'users-id', params: { id: user.id, }, }">{{ user.username }}</nuxt-link>
+                </li>
+            </ol>
+        </div>
+
     </div>
 </template>
 
