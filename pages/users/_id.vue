36a5e643c0c5995ff88c6c8006ac5531f36e5a61
Shai Gerbi <ShaiGerbi@gmail.com>
2020-03-24 19:48:22 +0200

    Design user page

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="container">
 
+        <div class="box">
+            <h3>Basic Data</h3>
+            <p>Name: name</p>
+            <p>Username: username</p>
+            <p>Website: <a href="#">website</a></p>
+        </div>
+
+        <div class="box">
+            <h3>Communication</h3>
+            <p>Email: <a href="#">email</a></p>
+            <p>Phone: <a href="#">phone</a></p>
+        </div>
+
     </div>
 </template>
 
