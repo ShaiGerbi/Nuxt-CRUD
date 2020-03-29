eb62659bd38856e3b8ac6f43973fc67534c1e736
Shai Gerbi <ShaiGerbi@gmail.com>
2020-03-29 22:31:19 +0300

    Create loading component

---
+++ components/Loading.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div class="loading"></div>
+
+</template>
+
+<style scoped>
+
+    .loading {
+        height: 50px;
+        width: 50px;
+        border: #35495e dotted 5px;
+        border-radius: 50%;
+
+        animation-name: roll;
+        animation-duration: 3s;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+    }
+
+    @keyframes roll {
+        0% {
+            transform: rotate(0);
+            border-color: #64b5f6;
+        }
+
+        100% {
+            transform: rotate(360deg);
+            border-color: #ff7043 ;
+        }
+    }
+
+</style>
